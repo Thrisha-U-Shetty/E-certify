463b6b136e75c5f4bed577e920643ca4b0bd0154
Thrisha-U-Shetty <thrishashetty009@gmail.com>
2025-08-19 20:49:52 +0530

    Created a Landing page and tailwind css setup

--- frontend/e-certify-app/src/App.jsx
+++ frontend/e-certify-app/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { ethers } from 'ethers';
 import UploadCertificate from './Components/UploadCertificate';
+import LandingPage from './components/LandingPage';
 
 function App() {
   const [account, setAccount] = useState(null);
@@ -19,13 +20,18 @@ function App() {
   };
 
   return (
-    <div style={{ padding: "20px" }}>
-      <h1>E-Certify</h1>
-      <UploadCertificate/>
-      <button onClick={connectWallet}>
-        {account ? `Connected: ${account}` : "Connect MetaMask"}
-      </button>
-    </div>
+    <>
+    <LandingPage/>
+    </>
+
+    // <div style={{ padding: "20px" }}>
+
+    //   <h1>E-Certify</h1>
+    //   <UploadCertificate/>
+    //   <button onClick={connectWallet}>
+    //     {account ? `Connected: ${account}` : "Connect MetaMask"}
+    //   </button>
+    // </div>
   );
 }
 
