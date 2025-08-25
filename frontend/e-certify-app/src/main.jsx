2d3599698719b494a3d3651585ea0f81f9b7db06
Thrisha-U-Shetty <thrishashetty009@gmail.com>
2025-08-25 20:39:15 +0530

    Added email verification + welcome email feature

--- frontend/e-certify-app/src/main.jsx
+++ frontend/e-certify-app/src/main.jsx
@@ -1,13 +1,13 @@
-// src/main.jsx
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById("root")).render(
-
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</React.StrictMode>
 );
